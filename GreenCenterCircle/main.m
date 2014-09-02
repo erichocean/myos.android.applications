dc4e6eba7ea913de960943579d5aeed110e9b3a8
Amr Aboelela <amraboelela@gmail.com>
2014-09-02 15:31:28 -0700

    Added other Rectangle and Circle apps

---
+++ GreenCenterCircle/main.m
@@ -0,0 +1,9 @@
+
+#import <UIKit/UIKit.h>
+
+#pragma mark - Static functions
+
+int main(int argc, char *argv[])
+{
+    return UIApplicationMain(argc, argv, nil, @"AppDelegate");
+}
--- RedRectangle/install.sh
+++ RedRectangle/install.sh
@@ -4,5 +4,5 @@
 
 CAN_CLEAN=YES
 
-TARGET=MyOSApplication
+TARGET=myOSApplication
 source ${MY_FRAMEWORKS_PATH}/resources/nativeApp-install.sh
