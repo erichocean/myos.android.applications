ce8a453094e930bab23dacc91dc1c36072561055
Amr Aboelela <amraboelela@gmail.com>
2014-09-02 14:22:43 -0700

    Deleted .so files

--- myApps/bin/classes.dex.d
+++
@@ -1,6 +0,0 @@
-/Users/amraboelela/develop/myos/android/applications/myApps/bin/classes.dex : \
-/Users/amraboelela/develop/myos/android/applications/myApps/bin/classes/com/myos/myapps/BuildConfig.class \
-/Users/amraboelela/develop/myos/android/applications/myApps/bin/classes/com/myos/myapps/myAppsNA.class \
-/Users/amraboelela/develop/myos/android/applications/myApps/bin/classes/com/myos/myapps/R$attr.class \
-/Users/amraboelela/develop/myos/android/applications/myApps/bin/classes/com/myos/myapps/R$string.class \
-/Users/amraboelela/develop/myos/android/applications/myApps/bin/classes/com/myos/myapps/R.class \
