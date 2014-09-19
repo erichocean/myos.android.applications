dca2f1fc9b67fa1ee39aea9244123dd430418cf0
Amr Aboelela <amraboelela@gmail.com>
2014-09-19 08:53:01 -0700

    Removed myApps

--- myApps/FileManager.h
+++
@@ -1,7 +0,0 @@
-/*
- * Copyright 2014 myOS Group. All rights reserved.
- *
- */
-
-void FileManagerSetupDirectories();
-NSMutableArray *FileManagerInstantiateApps();
--- myApps/run.sh
+++
@@ -1,9 +0,0 @@
-#
-# Copyright 2014 myOS Group. All rights reserved.
-#
-
-CAN_CLEAN=YES
-
-TARGET=NativeActivity
-source ${MY_FRAMEWORKS_PATH}/resources/common-install.sh
-source ${MY_FRAMEWORKS_PATH}/resources/common-run.sh
