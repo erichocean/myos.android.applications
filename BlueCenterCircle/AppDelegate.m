0faf1dffe671cad4436b382b5022b0c0d7a37099
Amr Aboelela <amraboelela@gmail.com>
2014-09-25 16:38:49 -0700

    Launched BlueCenterCircle app

--- BlueCenterCircle/AppDelegate.m
+++ BlueCenterCircle/AppDelegate.m
@@ -18,26 +18,12 @@
     self.window = [[[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]] autorelease];
     //DLog(@"window: %@", window);
     self.window.backgroundColor = [UIColor brownColor];
-    //DLog(@"self.window.backgroundColor: %@", self.window.backgroundColor);
-//    UITextField* display = [[[UITextField alloc] initWithFrame:CGRectMake(10, 20, 70, 30)] autorelease];
-//        [display setEditable: NO];
-//    [display setBackgroundColor:[UIColor blueColor]];
-    //[display setDrawsBackground: YES];
-//        [display setAlignment: NSRightTextAlignment];
 
-//    display.text = @"Red Rectangle";
-//    [window addSubview: display];
-
-    CircleView *solidView = [[[CircleView alloc] initWithFrame:CGRectMake(0, 40, 170, 150)] autorelease];
-    //DLog(@"solidView: %@", solidView);
-    //solidView.backgroundColor = [UIColor redColor];
-    [self.window addSubview:solidView];
+    CGRect screenBounds = [[UIScreen mainScreen] bounds];
+    CircleView *circleView = [[[CircleView alloc] initWithFrame:CGRectMake((screenBounds.size.width - kCircleSize) / 2, (screenBounds.size.height - kCircleSize) / 2.0, kCircleSize, kCircleSize)] autorelease];
+    [self.window addSubview:circleView];
     
-    //[NSTimer scheduledTimerWithTimeInterval:2.0 target:self selector:@selector(addSolidView:) userInfo:nil repeats:NO];
     [self.window makeKeyAndVisible];
-    //DLog();
-//    CircleView* solidView2 = [[[CircleView alloc] initWithFrame:CGRectMake(0, 0, 1, 1)] autorelease];
-//    [solidView addSubview:solidView2];
     return YES;
 }
 
@@ -48,13 +34,13 @@
 }
 
 #pragma mark - Delegates
-
+/*
 - (void)addSolidView:(NSTimer *)timer
 {
     CircleView *solidView = [[[CircleView alloc] initWithFrame:CGRectMake(150, 230, 100, 100)] autorelease];
     DLog(@"solidView: %@", solidView);
     //solidView.backgroundColor = [UIColor redColor];
     [self.window addSubview:solidView];
-}
+}*/
 
 @end
